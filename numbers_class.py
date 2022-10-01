e5ec36360a76171d8454e2ea625b520ddcd89085
achandra22 <achandra220@gmail.com>
2022-10-01 16:30:06 -0400

    added numbers to fish

--- numbers_class.py
+++ numbers_class.py
@@ -26,15 +26,20 @@ class numbers(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
 
+        # the objects mathematical value
+        self.val = str(random.randrange(1, 9))
+
+        # add number to the image
+        self.font = pygame.font.SysFont("comicsansms", 32)
+        self.text = self.font.render(self.val, True, (255, 255, 255))
+        self.image.blit(self.text, (0, 0))
+
         # starting position
         self.rect.left = WIDTH
         self.rect.top = random.randrange(32, HEIGHT - 32)
 
         # speeds
         self.speed = random.randrange(1, 5)
-
-        # the objects mathematical value
-        self.val = str(random.randrange(1, 9))
 
     def update(self):
         self.rect.x -= self.speed
