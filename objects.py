0be03df087bf93578c758f612f42f7d5dd51d870
hailbee <98438448+hailbee@users.noreply.github.com>
2022-10-01 13:36:05 -0400

    Update objects.py

--- objects.py
+++ objects.py
@@ -5,6 +5,7 @@ Created on Sat Oct  1 08:24:11 2022
 @author: hbori
 """
 import pygame
+import random
 
 WIDTH = 1050  # width of our game window
 HEIGHT = 700  # height of our game window
@@ -62,3 +63,26 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 0
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
+            
+class numbers(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = pygame.image.load('fish.png').convert()
+        self.image.set_colorkey(BLACK)
+        
+        self.rect = self.image.get_rect()
+        
+        # starting position
+        self.rect.left = WIDTH
+        self.rect.top = random.randrange(32, HEIGHT - 32)
+        
+        # speeds
+        self.speed = random.randrange(1, 5)
+        
+        # the objects mathematical value
+        self.val = random.randrange(1, 9)
+        
+    def update(self):
+        self.rect.x -= self.speed
+        
