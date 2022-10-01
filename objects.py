ae05886cc169dea50db9d7c89b2708387b0b0784
achandra22 <achandra220@gmail.com>
2022-10-01 13:07:58 -0400

    updated background image

--- objects.py
+++ objects.py
@@ -6,38 +6,39 @@ Created on Sat Oct  1 08:24:11 2022
 """
 import pygame
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 1200  # width of our game window
+HEIGHT = 800  # height of our game window
 
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 
-# player_img = 
+# player_img =
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        
-        self.image = pygame.image.load('starfish.png').convert()
+
+        self.image = pygame.image.load("starfish.png").convert()
         self.image.set_colorkey(BLACK)
-        
+
         self.rect = self.image.get_rect()
-        
+
         # starting position
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        
+
         # speeds
         self.speed = 5
-        
+
     def update(self):
-        
+
         # updating position
-        
+
         self.speedx = 0
         self.speedy = 0
-        
+
         keystate = pygame.key.get_pressed()
-        
+
         if keystate[pygame.K_LEFT]:
             self.speedx = -8
         if keystate[pygame.K_RIGHT]:
@@ -46,22 +47,18 @@ class Player(pygame.sprite.Sprite):
             self.speedy = 8
         if keystate[pygame.K_UP]:
             self.speedy = -8
-            
-            
+
         self.rect.x += self.speedx
         self.rect.y += self.speedy
-        
+
         # wrapping the object around
-        
+
         if self.rect.left > WIDTH:
             self.rect.right = 0
         if self.rect.right < 0:
             self.rect.left = WIDTH
-            
+
         if self.rect.top < 0:
             self.rect.top = 0
         if self.rect.bottom > HEIGHT:
             self.rect.bottom = HEIGHT
-        
-        
-        
--- starfish.py
+++ starfish.py
@@ -6,11 +6,10 @@ Created on Sat Oct  1 08:15:04 2022
 """
 
 import pygame
-import objects
+from objects import Player, WIDTH, HEIGHT
 
-WIDTH = 500  # width of our game window
-HEIGHT = 500 # height of our game window
-FPS = 30 # frames per second
+# import objects
+FPS = 30  # frames per second
 
 BLACK = (0, 0, 0)
 
@@ -23,39 +22,39 @@ clock = pygame.time.Clock()
 
 ### bg
 
-background = pygame.image.load('bg.jpg')
+background = pygame.image.load("bg.jpg")
+background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 bg_rect = background.get_rect()
 
 ### sprites
-
 all_sprites = pygame.sprite.Group()
 
-player = objects.Player()
+player = Player()
 all_sprites.add(player)
 
 # Game Loop
 running = True
 while running:
-    
+
     clock.tick(FPS)
-    
+
     ######## Process input (events)
-    
+
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
-            running = False    
-    
+            running = False
+
     ######## Update
-    
+
     all_sprites.update()
-    
+
     ######## Render (draw)
-    
+
     screen.fill(BLACK)
     screen.blit(background, bg_rect)
     all_sprites.draw(screen)
-    
+
     pygame.display.flip()
-    
+
 pygame.quit()
